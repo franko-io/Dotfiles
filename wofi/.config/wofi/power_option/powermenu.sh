ad4238ebfc5d04befd5fd0a2321bc97e71cf64e3
franko-io <onlyfranko.io@gmail.com>
2025-04-02 19:10:56 +0200

    Wofi - Initial power menu

---
+++ wofi/.config/wofi/power_option/powermenu.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+op=$( echo -e " Shutdown\n Reboot\n Suspend\n Lock\n Logout" | wofi -i --dmenu | awk '{print tolower($2)}' )
+
+case ${op} in
+    shutdown)
+        systemctl poweroff --now
+        ;;
+    reboot)
+        systemctl reboot --now
+        ;;
+    lock)
+        hyprlock
+        ;;
+    suspend)
+        systemctl suspend
+        ;;
+    logout)
+        hyprctl dispatch exit
+        ;;
+    hibernate)
+        systemctl hibernate
+        ;;
+esac
