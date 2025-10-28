9170658c08248a7b1a9e1b8667a03f93694c555a
franko-io <onlyfranko.io@gmail.com>
2025-10-28 17:11:48 +0100

    Nvim - Added Tmux navigator plugin

--- new-nvim/.config/new-nvim/lua/config/lazy.lua
+++ new-nvim/.config/new-nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ vim.g.maplocalleader = '\\'
 require('lazy').setup({
   spec = {
     { import = 'plugins.ui' },
+    { import = 'plugins.tmux' },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
