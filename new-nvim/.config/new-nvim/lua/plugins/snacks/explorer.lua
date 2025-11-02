b7319bed82617a269d35dae7beff7ed27388aa96
franko-io <onlyfranko.io@gmail.com>
2025-11-02 02:11:30 +0100

    Nvim - Included ignore files in snacks explorer

--- new-nvim/.config/new-nvim/lua/plugins/snacks/explorer.lua
+++ new-nvim/.config/new-nvim/lua/plugins/snacks/explorer.lua
@@ -1,12 +1,12 @@
 return {
   'folke/snacks.nvim',
-  ---@type snacks.Config
   -- TODO: Add keybind to automatically close explorer on select such as shift+enter
   opts = {
     picker = {
       sources = {
         explorer = {
           hidden = true,
+          ignored = true,
         },
       },
     },
