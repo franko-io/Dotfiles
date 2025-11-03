5004d1f86d822739545d9b2d17b117c7f74678b8
franko-io <onlyfranko.io@gmail.com>
2025-11-03 17:32:21 +0100

    Nvim - Add custom text object to mini.ai

--- nvim/.config/nvim/lua/plugins/code/mini.lua
+++ nvim/.config/nvim/lua/plugins/code/mini.lua
@@ -1,7 +1,13 @@
 return {
   'nvim-mini/mini.nvim',
   config = function()
-    require('mini.ai').setup({ n_lines = 500 })
+    require('mini.ai').setup({
+      n_lines = 500,
+      custom_textobjects = {
+        d = { '%d+' },
+        D = { '%d+%.?%d*' },
+      },
+    })
 
     require('mini.surround').setup({
       n_lines = 500,
