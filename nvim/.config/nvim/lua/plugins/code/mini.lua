9ea3c3080c115901fc79d4d1f681a12bf9782327
franko-io <onlyfranko.io@gmail.com>
2025-11-05 01:53:55 +0100

    Nvim - Added mini.trailspace

--- nvim/.config/nvim/lua/plugins/code/mini.lua
+++ nvim/.config/nvim/lua/plugins/code/mini.lua
@@ -15,5 +15,38 @@ return {
     })
 
     require('mini.pairs').setup()
+    require('mini.trailspace').setup()
+
+    -- Disable highlighting for dashboard
+    vim.b.minitrailspace_disable = true
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+      callback = function(args)
+        local ft = vim.bo[args.buf].filetype
+        if ft == 'snacks_dashboard' or ft == '' or ft == 'lazy' then
+          vim.b[args.buf].minitrailspace_disable = true
+          pcall(require('mini.trailspace').unhighlight)
+        else
+          vim.b[args.buf].minitrailspace_disable = false
+          pcall(require('mini.trailspace').highlight)
+        end
+      end,
+    })
+
+    -- Keymaps for mini.trailspace
+    vim.keymap.set('n', '<leader>he', function()
+      require('mini.trailspace').highlight()
+    end, { desc = 'Highlight trailing whitespace' })
+
+    vim.keymap.set('n', '<leader>hd', function()
+      require('mini.trailspace').unhighlight()
+    end, { desc = 'Unhighlight trailing whitespace' })
+
+    vim.keymap.set('n', '<leader>ht', function()
+      require('mini.trailspace').trim()
+    end, { desc = 'Trim trailing whitespace' })
+
+    vim.keymap.set('n', '<leader>hT', function()
+      require('mini.trailspace').trim_last_lines()
+    end, { desc = 'Trim last blank lines' })
   end,
 }
