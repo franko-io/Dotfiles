d652a693155ee5a0c562a595554029039a740e29
franko-io <onlyfranko.io@gmail.com>
2025-11-07 00:23:44 +0100

    Nvim - Upgraded treesitter with textobjects

--- nvim/.config/nvim/lua/plugins/code/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/code/treesitter.lua
@@ -3,6 +3,9 @@ return {
   branch = 'master',
   lazy = false,
   build = ':TSUpdate',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
   main = 'nvim-treesitter.configs',
   opts = {
     ensure_installed = { 'bash', 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline' },
@@ -22,5 +25,76 @@ return {
       additional_vim_regex_highlighting = false,
     },
     indent = { enable = true },
+
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['al'] = '@loop.outer',
+          ['il'] = '@loop.inner',
+          ['ai'] = '@conditional.outer',
+          ['ii'] = '@conditional.inner',
+        },
+        selection_modes = {
+          ['@parameter.outer'] = 'v',
+          ['@function.outer'] = 'V',
+          ['@class.outer'] = '<c-v>',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+          [']a'] = '@parameter.inner',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+          ['[a'] = '@parameter.inner',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+        goto_next = {
+          [']i'] = '@conditional.outer',
+          [']l'] = '@loop.outer',
+        },
+        goto_previous = {
+          ['[i'] = '@conditional.outer',
+          ['[l'] = '@loop.outer',
+        },
+      },
+      lsp_interop = {
+        enable = true,
+        border = 'none',
+        peek_definition_code = {
+          ['<leader>df'] = '@function.outer',
+          ['<leader>dF'] = '@class.outer',
+        },
+      },
+    },
   },
 }
