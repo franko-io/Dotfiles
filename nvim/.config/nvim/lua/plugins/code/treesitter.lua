5b13414d62a07694eda2304d71b758fc9d30aa23
franko-io <onlyfranko.io@gmail.com>
2025-11-24 15:32:17 +0100

    Nvim - Update treesitter binds

--- nvim/.config/nvim/lua/plugins/code/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/code/treesitter.lua
@@ -62,21 +62,21 @@ return {
         set_jumps = true,
         goto_next_start = {
           [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
+          [']c'] = '@class.outer',
           [']a'] = '@parameter.inner',
         },
         goto_next_end = {
           [']M'] = '@function.outer',
-          [']['] = '@class.outer',
+          [']C'] = '@class.outer',
         },
         goto_previous_start = {
           ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
+          ['[c'] = '@class.outer',
           ['[a'] = '@parameter.inner',
         },
         goto_previous_end = {
           ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
+          ['[C'] = '@class.outer',
         },
         goto_next = {
           [']i'] = '@conditional.outer',
