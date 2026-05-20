21d1e3c01771c8581ed10b3358fa1f2e2b6d43c9
franko-io <onlyfranko.io@gmail.com>
2026-05-20 02:46:34 +0200

    Nvim - Update gitsigns

--- nvim/.config/nvim/lua/plugins/git/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/git/gitsigns.lua
@@ -17,31 +17,38 @@ return {
       changedelete = { text = '▎' },
     },
     on_attach = function(buffer)
-      local gs = package.loaded.gitsigns
+      local gs = require('gitsigns')
 
       local function map(mode, l, r, desc)
         vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc, silent = true })
       end
 
       -- stylua: ignore start
-    map("n", "]g", function() if vim.wo.diff then vim.cmd.normal({ "]c", bang = true })
-                  else gs.nav_hunk("next") end end, "Next Hunk")
-    map("n", "[g", function() if vim.wo.diff then vim.cmd.normal({ "[c", bang = true })
-                  else gs.nav_hunk("prev") end end, "Prev Hunk")
+      -- Navigation
+      map('n', ']g', function() if vim.wo.diff then vim.cmd.normal({ ']c', bang = true }) else gs.nav_hunk('next') end end, 'Next Hunk')
+      map('n', '[g', function() if vim.wo.diff then vim.cmd.normal({ '[c', bang = true }) else gs.nav_hunk('prev') end end, 'Prev Hunk')
+      map('n', ']G', function() gs.nav_hunk('last') end, 'Last Hunk')
+      map('n', '[G', function() gs.nav_hunk('first') end, 'First Hunk')
 
-    map("n", "]G", function() gs.nav_hunk("last") end, "Last Hunk")
-    map("n", "[G", function() gs.nav_hunk("first") end, "First Hunk")
-    map({ "n", "x" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-    map({ "n", "x" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-    map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-    map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-    map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-    map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
-    map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
-    map("n", "<leader>ghB", function() gs.blame() end, "Blame Buffer")
-    map("n", "<leader>ghd", gs.diffthis, "Diff This")
-    map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
-    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      -- Actions
+      map('n', '<leader>ghs', gs.stage_hunk, 'Stage/unstage Hunk')
+      map('n', '<leader>ghr', gs.reset_hunk, 'Reset Hunk')
+      map('v', '<leader>ghs', function() gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, 'Stage Hunk')
+      map('v', '<leader>ghr', function() gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end, 'Reset Hunk')
+      map('n', '<leader>ghS', gs.stage_buffer, 'Stage Buffer')
+      map('n', '<leader>ghR', gs.reset_buffer, 'Reset Buffer')
+      map('n', '<leader>ghp', gs.preview_hunk_inline, 'Preview Hunk Inline')
+      map('n', '<leader>ghP', gs.preview_hunk, 'Preview Hunk Popup')
+      map('n', '<leader>ghb', function() gs.blame_line({ full = true }) end, 'Blame Line')
+      map('n', '<leader>ghB', gs.blame, 'Blame Buffer')
+      map('n', '<leader>ghd', function() gs.diffthis(nil, { vertical = true, split = 'belowright' }) end, 'Diff This')
+      map('n', '<leader>ghD', function() gs.diffthis('~', { vertical = true, split = 'belowright' }) end, 'Diff This ~')
+      map('n', '<leader>ghq', gs.setqflist, 'Hunks to Quickfix')
+      map('n', '<leader>ghQ', function() gs.setqflist('all') end, 'All Hunks to Quickfix')
+
+      -- Toggles
+      map('n', '<leader>gtb', gs.toggle_current_line_blame, 'Toggle Line Blame')
+      map('n', '<leader>gtw', gs.toggle_word_diff, 'Toggle Word Diff')
       -- stylua: ignore end
     end,
   },
