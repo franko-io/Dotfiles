02fa63d42c4e5e1b53a6f4d2325fc749510f7371
franko-io <onlyfranko.io@gmail.com>
2026-05-25 01:34:16 +0200

    Hyprland - Move windows and workspaces to separate file

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -4,6 +4,7 @@ require('input')
 require('env-vars')
 require('keybinds')
 require('look-and-feel')
+require('windows-and-workspaces')
 
 ----------------
 ----  MISC  ----
@@ -11,57 +12,14 @@ require('look-and-feel')
 
 hl.config({
   misc = {
-    force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
-    disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
-  },
-})
-
---------------------------------
----- WINDOWS AND WORKSPACES ----
---------------------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
--- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
-
--- Example window rules that are useful
-
-local suppressMaximizeRule = hl.window_rule({
-  -- Ignore maximize requests from all apps. You'll probably like this.
-  name = 'suppress-maximize-events',
-  match = { class = '.*' },
-
-  suppress_event = 'maximize',
-})
--- suppressMaximizeRule:set_enabled(false)
-
-hl.window_rule({
-  -- Fix some dragging issues with XWayland
-  name = 'fix-xwayland-drags',
-  match = {
-    class = '^$',
-    title = '^$',
-    xwayland = true,
-    float = true,
-    fullscreen = false,
-    pin = false,
+    force_default_wallpaper = 1,
+    disable_hyprland_logo = true,
+    disable_splash_rendering = true,
+    focus_on_activate = true,
+    on_focus_under_fullscreen = 1,
   },
 
-  no_focus = true,
-})
-
--- Layer rules also return a handle.
--- local overlayLayerRule = hl.layer_rule({
---     name  = "no-anim-overlay",
---     match = { namespace = "^my-overlay$" },
---     no_anim = true,
--- })
--- overlayLayerRule:set_enabled(false)
-
--- Hyprland-run windowrule
-hl.window_rule({
-  name = 'move-hyprland-run',
-  match = { class = 'hyprland-run' },
-
-  move = '20 monitor_h-120',
-  float = true,
+  binds = {
+    hide_special_on_workspace_change = true,
+  },
 })
