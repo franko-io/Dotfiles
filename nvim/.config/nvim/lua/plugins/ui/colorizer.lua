e144a333e2ae0341098f51e008b60d9c0d234001
franko-io <onlyfranko.io@gmail.com>
2026-05-25 02:11:06 +0200

    Nvim - Enable colorizer rgb_fn

--- nvim/.config/nvim/lua/plugins/ui/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorizer.lua
@@ -7,7 +7,7 @@ return {
       RRGGBB = true, -- #RRGGBB hex codes
       names = true, -- "Name" codes like Blue
       RRGGBBAA = false, -- #RRGGBBAA hex codes
-      rgb_fn = false, -- CSS rgb() and rgba() functions
+      rgb_fn = true, -- CSS rgb() and rgba() functions
       hsl_fn = false, -- CSS hsl() and hsla() functions
       css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
       css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
